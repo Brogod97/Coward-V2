bc44507e50db594ca51bf06366768a89188d2655
Brogod97 <evergreen1286@gmail.com>
2024-02-19 22:53:03 +0900

    Init Project

---
+++ src/main/java/brogod/cowardv2/Cowardv2Application.java
@@ -0,0 +1,13 @@
+package brogod.cowardv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cowardv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cowardv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/brogod/cowardv2/Cowardv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package brogod.cowardv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cowardv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
